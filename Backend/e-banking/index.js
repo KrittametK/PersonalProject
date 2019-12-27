73d19cec156697634d2d3c48c846be8ee8f3d56e
Krittamet <k.koomueang@gmail.com>
2019-12-27 11:37:35 +0700

    [Added] add create/get user

--- Backend/e-banking/index.js
+++ Backend/e-banking/index.js
@@ -5,10 +5,39 @@ const db = require('./models')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-db.sequelize.sync({froce: true})
+db.sequelize.sync({froce: false})
 .then(()=>{
+
+  app.get("/getUsers", (req,res) => {
+    db.user.findAll()
+    .then(result => {
+      res.status(200).send(result) 
+    })
+    .catch(err => {
+      res.status(400).json({
+        message: err.message
+      })
+    })
+  })
+
+  app.post("/createUser", (req, res) => {
+    db.user.create({
+      name: req.body.name,
+      lastname: req.body.lastname,
+      username: req.body.username,
+      password: req.body.password
+    })
+    .then(result => {
+      res.status(201).send("create user sucess")
+    })
+    .catch(result => {
+      res.status(400).json({
+        message: err.message
+      })
+    })
+  })
+
   app.listen(8080, () => {
     console.log('server start and listen on port 8080')
   })
-
 })
